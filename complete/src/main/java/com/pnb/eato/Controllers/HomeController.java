429dfc75937782d2261f9f8b4ce5098a46320d20
byan1197 <Bondyan97@gmail.com>
2018-03-25 15:16:35 -0400

    added some blank controllers and requests

---
+++ complete/src/main/java/com/pnb/eato/Controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.pnb.eato.Controllers;
+
+import com.pnb.eato.Request.RequestMappings;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(RequestMappings.INDEX)
+    public void homepage(){
+
+    }
+
+}
--- complete/src/main/java/com/pnb/eato/hello/GreetingController.java
+++ complete/src/main/java/com/pnb/eato/hello/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "<h1 color='green'>Hello, %s!</h1>";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")

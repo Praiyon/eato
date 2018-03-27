bf273da6d598b910fddf48dbe8c6239e9c65fcd4
byan1197 <Bondyan97@gmail.com>
2018-03-27 16:58:39 -0400

    added index

--- complete/src/main/java/com/pnb/eato/Controllers/HomeController.java
+++ complete/src/main/java/com/pnb/eato/Controllers/HomeController.java
@@ -1,20 +1,24 @@
 package com.pnb.eato.Controllers;
 
+import com.pnb.eato.DatabaseTraverser.RestaurantDBService;
+import com.pnb.eato.Models.Restaurant;
 import com.pnb.eato.Request.RequestMappings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 public class HomeController {
 
+    @Autowired
+    RestaurantDBService restService;
+
     @RequestMapping(RequestMappings.INDEX)
-    public void homepage(){
-
+    public List<Restaurant> homepage() throws SQLException {
+        return restService.getAllRestaurants();
     }
-
-//    @RequestMapping("/error")
-//    public void error(){
-//
-//    }
-
 }

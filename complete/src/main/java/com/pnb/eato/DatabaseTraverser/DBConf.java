c855bb77fd18584f7f2f2dafb0214ed0d00cf8b3
praiyon <praiyon@gmail.com>
2018-03-28 15:33:00 -0400

    api stuff

--- complete/src/main/java/com/pnb/eato/DatabaseTraverser/DBConf.java
+++ complete/src/main/java/com/pnb/eato/DatabaseTraverser/DBConf.java
@@ -11,13 +11,6 @@ import java.sql.SQLException;
 @Configuration
 public class DBConf {
 
-//     @Value("${eato.db.user}")
-//     private String username;
-//     @Value("${eato.db.password}")
-//     private String password;
-//     @Value("${eato.db.connection}")
-//     private String connection;
-
      @Bean
      public Connection connection() throws SQLException {
           return DriverManager.getConnection("jdbc:postgresql://web0.site.uottawa.ca:15432/pkuma076",

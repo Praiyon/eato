b2c6c8cdc53d07140dfeae5b1f2a8680540c2e3c
praiyon <praiyon@gmail.com>
2018-03-30 21:32:08 -0400

    minor controller change

--- complete/src/main/java/com/pnb/eato/Controllers/RestaurantController.java
+++ complete/src/main/java/com/pnb/eato/Controllers/RestaurantController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.bind.ValidationException;
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 public class RestaurantController {
@@ -19,12 +20,36 @@ public class RestaurantController {
     @Autowired
     private RestaurantDBService restauService;
 
+//    @RequestMapping(value= RequestMappings.RESTAU, method= RequestMethod.GET)
+//    public Restaurant restaurantGet(@RequestParam("restId") int restId) throws SQLException, ValidationException {
+//        Restaurant restaurant = restauService.queryById(restId).stream().findFirst().orElse(null);
+//        if (restaurant == null){
+//            throw new ValidationException("oh no!");
+//        }
+//        return restaurant;
+//    }
+
     @RequestMapping(value= RequestMappings.RESTAU, method= RequestMethod.GET)
-    public Restaurant restaurantGet(@RequestParam("restId") int restId) throws SQLException, ValidationException {
-        Restaurant restaurant = restauService.queryById(restId).stream().findFirst().orElse(null);
-        if (restaurant == null){
-            throw new ValidationException("oh no!");
+    public List<Restaurant> restaurantGetByName(@RequestParam(value = "name", required = false) String name) throws SQLException, ValidationException {
+        List<Restaurant> restaurants;
+        if(name == null){
+            restaurants = restauService.getAllRestaurants();
+        }else{
+            restaurants = restauService.queryByName(name);
+            if (restaurants == null){
+                throw new ValidationException("oh no!");
+            }
         }
-        return restaurant;
+
+        return restaurants;
     }
+
+//    @RequestMapping(value= RequestMappings.RESTAU, method= RequestMethod.GET)
+//    public List<Restaurant> restaurantGetByName() throws SQLException, ValidationException {
+//        List<Restaurant> restaurants = restauService.getAllRestaurants();
+//        if (restaurants == null){
+//            throw new ValidationException("oh no!");
+//        }
+//        return restaurants;
+//    }
 }
